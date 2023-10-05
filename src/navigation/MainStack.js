a8d90c8924e741263be6846b79db39a06fd571ff
Hari sainath <harisainath51@gmail.com>
2023-10-05 09:40:17 +0530

    change structure

--- src/navigation/MainStack.js
+++ src/navigation/MainStack.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../containers/Home'
+import AccountPreview from '../containers/AccountPreview';
 const Stack = createNativeStackNavigator();
 const MainStack = () => {
   return (
     <Stack.Navigator >
         <Stack.Screen options={{headerShown:false}} name="Home" component={Home}/>
+        <Stack.Screen options={{headerShown:false}} name="AccountPreview" component={AccountPreview}/>
     </Stack.Navigator>
   )
 }
